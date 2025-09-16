d0d3a2aa1539284cdae35010142d0031f9309d84
Aditya Singh <aditya.ajaysingh2002@gmail.com>
2025-09-16 18:15:04 +0530

    Update settings.py

--- libraryms/settings.py
+++ libraryms/settings.py
@@ -60,6 +60,19 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'libraryms.urls'
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 TEMPLATES = [
     {
