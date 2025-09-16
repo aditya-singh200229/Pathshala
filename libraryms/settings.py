c71f6210678ae776635963fd81b505885b76edbf
Aditya Singh <aditya.ajaysingh2002@gmail.com>
2025-09-16 18:11:48 +0530

    Update settings.py

--- libraryms/settings.py
+++ libraryms/settings.py
@@ -82,14 +82,9 @@ WSGI_APPLICATION = 'libraryms.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST', default='localhost'),
-        'PORT': config('DB_PORT', default='5432'),
-    }
+        'default': dj_database_url.config(
+        default=config("DATABASE_URL")
+    )
 }
 
 
